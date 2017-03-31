10be9b70b7d2b2dd43e48db685b7d031140d49d8
Milorad <miloradst@hotmail.com>
2017-03-31 22:38:58 +0200

    korisnici

--- src/model/users/User.java
+++ src/model/users/User.java
@@ -12,6 +12,25 @@ public class User extends AbstractUser{
 		setViewPosts(true);
 	}
 	
+	public User(String username, String password, String name, String surname, String email){
+		
+		super(username, password, name, surname, email);
+
+		setCreateLocation(true);
+		setCreateProblem(true);
+		setCreateRedAlertProblem(true);
+		setDonate(true);
+		setViewPosts(true);
+	}
+	
+
+	@Override
+	public int compareTo(AbstractUser user) {
+		if (user.getNumberOfVotes() < getNumberOfVotes()) return 1;
+		else if (user.getNumberOfVotes() < getNumberOfVotes()) return -1;
+		else return 0;
+	}
+	
 	
 	
 }
