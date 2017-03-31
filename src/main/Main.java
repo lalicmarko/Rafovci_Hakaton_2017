496a5391450e15a3318a7d29d4cffd90040bfa7c
Marko Lalić <mlalic995@gmail.com>
2017-03-31 22:28:50 +0200

    Komitovanje

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,13 +8,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		MainFrame mainFrame = MainFrame.getInstance();
-<<<<<<< HEAD
 		
 		NewProblemWindow npw = new NewProblemWindow();
 		npw.setVisible(true);
-=======
 		mainFrame.setVisible(true);
->>>>>>> origin/master
 		
 	}
 }
